1cea1f4bbe0c050bb5ca109c145ecebddc2722df
John Dillon Easter <johndilloneaster@gmail.com>
2018-08-13 13:06:58 -0700

    Ran through exercise

---
+++ src/components/CartHeader.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const CartHeader = () => {
+  return (<nav className="navbar navbar-dark bg-primary">
+            <a className="navbar-brand" href="#">Shopping Cart</a>
+          </nav>)
+}
+
+export default CartHeader
---
+++ src/components/CartItems.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const CartItems = () => {
+  return (<div className="container">
+            <h1>Items</h1>
+          </div>)
+}
+
+export default CartItems
